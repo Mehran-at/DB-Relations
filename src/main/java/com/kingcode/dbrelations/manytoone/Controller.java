6a3a346e365c4fb0fe8dc72d4858ee688e5b547b
Mehran Hosseini <mehran.hosseini@ebcont.com>
2022-04-19 15:35:00 +0200

    Fix script is null issue

--- src/main/java/com/kingcode/dbrelations/manytoone/Controller.java
+++ src/main/java/com/kingcode/dbrelations/manytoone/Controller.java
@@ -3,7 +3,9 @@ package com.kingcode.dbrelations.manytoone;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 @RestController
 public class Controller {
@@ -13,6 +15,24 @@ public class Controller {
     @GetMapping("/home")
     public String get() {
         return "Home Wel";
+    }
+
+    @GetMapping("/addcity")
+    public City saveCities() {
+        City city = new City();
+        Country country = new Country();
+
+        city.setCityName("Vienna");
+        city.setCountry(country);
+
+        country.setCountryName("Austria");
+        country.setId(1L);
+        Set<City> cities = new HashSet<>();
+        country.setCities(cities);
+
+        cities.add(city);
+        System.out.println("=========================");
+        return service.save(city);
     }
 
     @PostMapping("/addcity")
